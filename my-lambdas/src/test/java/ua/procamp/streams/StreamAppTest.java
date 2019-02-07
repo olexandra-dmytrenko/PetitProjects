f0bcbc38dea5026216e6b76a269f5a51c88d0cb6
Olexandra <olexandra.dmytrenko@gmail.com>
2019-02-07 21:18:58 +0200

    Added my implementation of lamdas in Java for GL classes

---
+++ my-lambdas/src/test/java/ua/procamp/streams/StreamAppTest.java
@@ -0,0 +1,44 @@
+package ua.procamp.streams;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.procamp.streams.stream.AsIntStream;
+import ua.procamp.streams.stream.IntStream;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class StreamAppTest {
+    
+    private IntStream intStream;
+
+    @Before
+    public void init() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+    }
+    
+    @Test
+    public void testStreamOperations() {
+        System.out.println("streamOperations");
+        int expResult = 42;
+        int result = StreamApp.streamOperations(intStream);
+        assertEquals(expResult, result);        
+    }
+
+    @Test
+    public void testStreamToArray() {
+        System.out.println("streamToArray");
+        int[] expResult = {-1, 0, 1, 2, 3};
+        int[] result = StreamApp.streamToArray(intStream);
+        assertArrayEquals(expResult, result);        
+    }
+
+    @Test
+    public void testStreamForEach() {
+        System.out.println("streamForEach");
+        String expResult = "-10123";
+        String result = StreamApp.streamForEach(intStream);
+        assertEquals(expResult, result);        
+    }
+}
