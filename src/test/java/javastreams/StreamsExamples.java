2c57baa35e3dacd83d796559b947a64dde28a5ad
Oleksandra_Dmytrenko <DevIT0319>
2021-01-16 11:48:17 +0200

    [STREAMS FOR EXAM] Added several streams for TEF exam 2020 / 2021

--- src/test/java/javastreams/StreamsExamples.java
+++ src/test/java/javastreams/StreamsExamples.java
@@ -1,9 +1,11 @@
 package javastreams;
 
 import org.junit.Test;
+import org.testng.Assert;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -16,6 +18,23 @@ public class StreamsExamples {
         List<String> list = Stream.of("1", "2", "3")
                 .map(a -> a + a)
                 .collect(Collectors.toList());
+
+        final String prayer = Stream.of("I", "pray", "to", "God", "for", "help")
+                .collect(Collectors.joining(" ", "", "."));
+        Assert.assertEquals("I pray to God for help.", prayer);
+
+        double averageLength = Stream.of("I", "pray", "to", "God", "for", "help")
+                .mapToInt(String::length)
+                .average()
+                .orElse(0);
+
+
+        final double averageLength1 = Stream.of("I", "pray", "to", "God", "for", "help")
+                .map(s -> s.length()).reduce(0, (a, b) -> a + b).doubleValue() / 5;
+
+
+        Assert.assertEquals(2.83, averageLength, 0.5);
+        Assert.assertEquals(3.0, averageLength1, 0.5);
 
         List<Integer> list1 = Stream.of(1, 2, 3)
                 .map(a -> a + a)
