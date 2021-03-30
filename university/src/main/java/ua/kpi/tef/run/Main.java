8ec111240b4d908ccc0f3c17f6e90502c25edebd
Oleksandra_Dmytrenko <DevIT0319>
2021-03-30 12:59:06 +0300

    [Logger] Added log4j2 and slf4j

--- university/src/main/java/ua/kpi/tef/run/Main.java
+++ university/src/main/java/ua/kpi/tef/run/Main.java
@@ -3,13 +3,18 @@ package ua.kpi.tef.run;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+import lombok.extern.slf4j.Slf4j;
 import ua.kpi.tef.context.Application;
 import ua.kpi.tef.controller.DepartmentController;
 
 //to start your controller
+@Slf4j
 public class Main {
+    // private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
 
     public static void main(String[] args) {
+        log.info("Start Application");
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
 
         final ConfigurableEnvironment environment = context.getEnvironment();
